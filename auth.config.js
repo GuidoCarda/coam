b0e0405844d40657eec2180d148bf38268265c0d
joaquinvesapa <joaquinvesapa@gmail.com>
2024-06-26 23:51:17 -0300

    google login

--- auth.config.js
+++ auth.config.js
@@ -3,12 +3,13 @@ import { defineConfig } from "auth-astro";
 
 console.log(import.meta.env.GOOGLE_SECRET_ID)
 export default defineConfig({
-  providers: [
-    Google({
-    clientId: import.meta.env.GOOGLE_CLIENT_ID,
-    clientSecret: import.meta.env.GOOGLE_SECRET_ID
-  })],
-  callbacks: {
+	providers: [
+		Google({
+			clientId: import.meta.env.AUTH_GOOGLE_ID,
+			clientSecret: import.meta.env.AUTH_GOOGLE_SECRET
+		})],
+	secret: process.env.AUTH_SECRET,
+	callbacks: {
 		session: ({ session, token }) => ({
 			...session,
 			user: {
