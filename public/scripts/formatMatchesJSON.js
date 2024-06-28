cb6a55bf951956bc3fce80c655ae2ee9cf9fd06d
joaquinvesapa <joaquinvesapa@gmail.com>
2024-06-28 20:41:56 -0300

    format matches for missing ids

--- public/scripts/formatMatchesJSON.js
+++ public/scripts/formatMatchesJSON.js
@@ -1,6 +1,6 @@
 import matches from '../matches.json' assert {type: "json"}
 import teamsv2 from '../teamsv2.json' assert {type: "json"}
-import {open} from 'node:fs/promises'
+import { open } from 'node:fs/promises'
 
 
 // matches.forEach((match, i) => {
@@ -15,14 +15,15 @@ import {open} from 'node:fs/promises'
 // matches.forEach((match, i) => {
 //   match.team_data = [teamDataHome, teamDataAway]
 // })
-matches.filter(match => match.team_data[0].score !== '').forEach((match, i) => {
+teamsv2.forEach(team =>
+  matches.filter(match => match.team_data[0].teamref.includes(team.teamRef)).forEach((match, i) => {
     let idTeamHome = teamsv2.filter(team => team.teamRef === match.team_data[0].teamref)[0].id
     let idTeamAway = teamsv2.filter(team => team.teamRef === match.team_data[1].teamref)[0].id
     match.team_data[0].team_id = idTeamHome
     match.team_data[1].team_id = idTeamAway
-    // console.log(match.team_data)
+    console.log(match.team_data)
   })
-
+)
 // console.log(match.team_data)
 
 const matchesBuffer = Buffer.from(JSON.stringify(matches));
